04ea1558a3253904f1dac9ff1b21acf54afe5316
noboru-i <ishikura.noboru@gmail.com>
2015-06-11 13:13:30 +0900

    implemented

---
+++ lib/findbugs_translate_checkstyle_format/translate.rb
@@ -0,0 +1,38 @@
+require 'nori'
+
+module FindbugsTranslateCheckstyleFormat
+  module Translate
+    def parse(xml)
+      Nori
+        .new(parser: :rexml)
+        .parse(xml)
+    end
+
+    def trans(xml)
+      require 'rexml/document'
+      doc = REXML::Document.new
+      doc << REXML::XMLDecl.new('1.0', 'UTF-8')
+
+      checkstyle = doc.add_element("checkstyle")
+      xml['BugCollection']['BugInstance'].each do |bugInstance|
+        file = checkstyle.add_element("file", {
+          'name' => fqcn_to_path(bugInstance['SourceLine']['@classname'], xml)
+          })
+        file.add_element("error", {
+          'line' => bugInstance['SourceLine']['@start'],
+          'severity' => '',
+          'message' => "[#{bugInstance['@category']}] #{bugInstance['LongMessage']}"
+          })
+      end
+
+      doc
+    end
+
+    def fqcn_to_path(fqcn, xml)
+      path = fqcn.gsub('.', '/') + '.java'
+      xml['BugCollection']['Project']['SrcDir'].find do |src|
+        src.index(path) != nil
+      end
+    end
+  end
+end
